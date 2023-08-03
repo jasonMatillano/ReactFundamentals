dbbe574a617b553d9983438b2b67d031a011c1bd
Jason <jason.matillano@gmail.com>
2023-08-03 14:03:15 +0800

    0.0.2.ConstVariables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+
+  const title  = 'My React App';
+  const likes = 50;
+  const person = {name : 'jason', age : 30};
+  const link = 'https://google.com';
+
   return (
     <div className="App">
       <div className="content"> 
-        <h1> app content </h1>
+        <h1> {title} </h1>
+        <p>Liked {likes} Times </p>
+
+        <p>My name is {person.name} im {person.age} </p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
