b8805b9182f6300253d6d363d79724777aa3a3c8
Jason <jason.matillano@gmail.com>
2023-08-03 18:34:21 +0800

    on0.0.7.onClickWithValuer

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = (name) => {
+        console.log('clicked' );
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('clicked again' + name); 
+    }
+
     return ( 
      <div className="home">
-        <h2>Homeaaaa</h2>
+        <h2>Home</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={() => handleClickAgain(' jason')}>
+        Click Me Again</button>
      </div>
      );
 }
