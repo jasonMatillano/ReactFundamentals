12163daca645a44b3f9591fa43451521ab07e291
Jason <jason.matillano@gmail.com>
2023-08-03 18:21:12 +0800

    added css in navbar

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import Home from './Home';
 
 function App() {
 
-  const title  = 'My React App';
-  const likes = 50;
-  const person = {name : 'jason', age : 30};
-  const link = 'https://google.com';
-
   return (
     <div className="App">
-      <Home></Home>
+      
       <Navbar></Navbar>
       <div className="content"> 
-        <h1> {title} </h1>
-        <p>Liked {likes} Times </p>
-
-        <p>My name is {person.name} im {person.age} </p>
-        <a href={link}>Google</a>
+        <Home></Home>
       </div>
     </div>
   );
