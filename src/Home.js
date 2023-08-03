b3fb6664975fe9f4a394d9b6bbf7cefd9475b76c
Jason <jason.matillano@gmail.com>
2023-08-03 18:41:09 +0800

    0.0.8.EventObject

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,17 @@
 const Home = () => {
-    const handleClick = (name) => {
-        console.log('clicked' );
+    const handleClick = (e) => {
+        console.log('clicked', e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('clicked again' + name); 
+    const handleClickAgain = (name,e) => {
+        console.log('clicked again' + name , e.target); 
     }
 
     return ( 
      <div className="home">
         <h2>Home</h2>
         <button onClick={handleClick}>Click Me</button>
-        <button onClick={() => handleClickAgain(' jason')}>
-        Click Me Again</button>
+        <button onClick={(e) => handleClickAgain(' jason', e)}>Click Me Again</button>
      </div>
      );
 }
