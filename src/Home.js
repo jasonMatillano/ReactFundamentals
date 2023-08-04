f090a982b83285061d64786cd22550e383dc571f
Jason <jason.matillano@gmail.com>
2023-08-04 16:56:00 +0800

    0.1.7.FectchingJsonDB

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(
-        [
-            {
-            id: 1,
-            title: 'First Post',
-            body: 'This is the body of the first post.',
-            author: 'John Doe'
-            },
-            {
-            id: 2,
-            title: 'Second Post',
-            body: 'This is the body of the second post.',
-            author: 'Jane Smith'
-            },
-            {
-            id: 3,
-            title: 'Third Post',
-            body: 'This is the body of the third post.',
-            author: 'Bob Johnson'
-            }
-        ]
-    );
+    const [blogs, setBlogs] = useState(null);
     
     const [name, setName] = useState('mario');
 
@@ -33,14 +12,20 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('use effect ran');
-    },[name]);// function triggers when value of [parameter] is changed
+        fetch('http://localhost:8000/blogs')
+            .then(respones => {
+                return respones.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+    },[]);
 
     return ( 
      <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+      {/* <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
       <button onClick={() => setName('luigi')}>Change Name</button>
-      <p>{name}</p>
+      <p>{name}</p> */}
      </div>
      );
 }
