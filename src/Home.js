0b0622c8d472afca8386e21496f699758ee87209
Jason <jason.matillano@gmail.com>
2023-08-04 09:10:53 +0800

    0.1.0.OutputingObjectArray

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,39 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(
+        [
+            {
+            id: 1,
+            title: 'First Post',
+            body: 'This is the body of the first post.',
+            author: 'John Doe'
+            },
+            {
+            id: 2,
+            title: 'Second Post',
+            body: 'This is the body of the second post.',
+            author: 'Jane Smith'
+            },
+            {
+            id: 3,
+            title: 'Third Post',
+            body: 'This is the body of the third post.',
+            author: 'Bob Johnson'
+            }
+        ]
+      );
     
-    const handleClick = () => {
-        setName('luigi');
-        console.log(name);
-    }
+
 
     return ( 
      <div className="home">
-        <h2>HomePage</h2>
-        <h3>{ name }</h3>
-        <button onClick= {handleClick} >Click Me</button>
-
+        {blogs.map(blog => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </div>
+        ))}
      </div>
      );
 }
