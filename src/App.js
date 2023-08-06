57e8a57ca0ffd8a6786af6a1a847f1785c8b459a
Jason <jason.matillano@gmail.com>
2023-08-06 20:52:46 +0800

    0.2.3.RouteParameters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
   return (
     <Router> 
       <div className="App">
-        
         <Navbar />
         <div className="content"> 
           <Switch>
@@ -17,6 +17,9 @@ function App() {
             </Route>
             <Route exact path="/create">
               <Create></Create>
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails></BlogDetails>
             </Route>
           </Switch>
         </div>
