1db6ca31f8273038d41a4597c0d631a0a31aceb2
Jason <jason.matillano@gmail.com>
2023-08-07 01:03:02 +0800

    0.2.9.DeleteBlogsInBlogDetailPage

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory(); // use history package to go back to chosen page
+
+  const handleDelete = () => { // delete blog from db
+    fetch('http://localhost:8000/blogs/' + id, {
+      method: 'DELETE',
+    })
+    .then(() => {
+      history.push('/') // redirect to home after deletion
+    })
+  }
 
   return (
     <div className="blog-details">
@@ -15,6 +26,7 @@ const BlogDetails = () => {
           <h2>{ blog.title + " id:" + id}</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
