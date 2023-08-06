df9e09000f80aecce0853366714e8bf4ef7672c8
Jason <jason.matillano@gmail.com>
2023-08-07 00:40:40 +0800

    0.2.7.CreateLoadinDelayInSummitWithClearButtonAfterSubmit

--- src/Create.js
+++ src/Create.js
@@ -4,18 +4,36 @@ const Create = () => {
     const [title , setTitle] = useState('')
     const [body , setBody] = useState('')
     const [auhtor , setAuthor] = useState('mario')
+    const [isPending, setIsPending] = useState(false)
+    const [feedback, setFeedback] = useState('')
 
-    // on submit save input data to blog object
-    const handleSubmit = (e) => {
+    const handleSubmit = (e) => {//function to handle the form submission
         e.preventDefault()
         const blog = {title, body, auhtor}
-        console.log(blog)
+
+        setIsPending(true)//set to true so that we can show the loading animation
+
+        setTimeout(() => {//set timeout to 1 second so that we can see the loading animation
+            fetch('http://localhost:8000/blogs',{ //this is the url we are going to send the data to
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(blog)
+            }).then(() => {
+                console.log('blog added')
+                console.log(blog)
+                console.log(isPending)
+                setIsPending(false)
+                setFeedback('successfully added blog')
+                
+            })
+        }, 1000)
+
     }
 
     return (  
         <div className="create">
          <h2>Add a New Blog</h2>
-         <form onSubmit={handleSubmit}>
+         <form>
             <label> Blog Title</label>
             <input 
             type="text"
@@ -38,10 +56,12 @@ const Create = () => {
                 <option value="mario">mario</option>
                 <option value="luigi">luigi</option>
             </select>
-            <button>Add Blog</button>
-            <p>{title}</p>
-            <p>{body}</p>
-            <p>{auhtor}</p>
+            {!isPending && <p><button onClick={handleSubmit}>Add Blog</button></p> }
+            {!isPending && <p><button onClick={() => setFeedback('')}>Clear</button></p> }
+            {isPending && <button disabled>Adding Blog...</button>}
+
+            <p>{feedback}</p>
+    
             
          </form>
         </div>              
