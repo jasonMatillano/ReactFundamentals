6b5c203018bd6e3f10f4cce99abc1b2f69e00e78
Jason <mat.lopez072023@gmail.com>
2024-04-06 17:20:29 +0800

    added changes

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,18 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('click');
+    const handleClick = (e) => {
+        console.log('click', e);
     }
 
-    const handleClickAgain = (name) => {
-        console.log('hello ' + name);
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
     }
 
     return ( 
         <div className="home">
             <h1>Homepage</h1>
             <button onClick={handleClick}>click me</button>
-            <button onClick={()=>handleClickAgain('mario')}>click me again</button>
+            <button onClick={(e)=>handleClickAgain('mario', e)}>click me again</button>
         </div>
      );
 }
