8a4c2e6ad1ed0bbf5132fa7ef9631b533639cd59
Jason <mat.lopez072023@gmail.com>
2024-04-06 17:14:10 +0800

    added changes mario

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('click');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name);
+    }
+
     return ( 
         <div className="home">
             <h1>Homepage</h1>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={()=>handleClickAgain('mario')}>click me again</button>
         </div>
      );
 }
